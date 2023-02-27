2d7b925c2d8ee42c4cc1c8fd846e14f79b80c1c6
Dejan Bosanac <dbosanac@redhat.com>
2023-02-27 16:31:18 +0100

    Example of creating OpenVex document

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
+use std::collections::HashSet;
+
+use chrono::Utc;
+use openvex::{Metadata, OpenVex, Status, Statement};
 use reqwest::blocking::Client;
 use graphql_client::{reqwest::post_graphql_blocking as post_graphql, GraphQLQuery};
 use anyhow::*;
+use crate::q4::AllCertifyVulnVulnerability::OSV;
 
 
 
@@ -14,8 +19,6 @@ pub struct Q4;
 
 
 fn main() -> Result<(), anyhow::Error> {
-    println!("Hello, world!");
-
     let variables = q4::Variables;
 
     let client = Client::new();
@@ -25,11 +28,67 @@ fn main() -> Result<(), anyhow::Error> {
 
     let response_data = response_body.data.expect("missing response data");
 
-    //println!("{}", serde_json::to_string_pretty(&response_body).unwrap());
-    println!("{:?}", response_data);
+    let mut vex = openvex();
 
-    //let response_data: q4::ResponseData = response_body.data.expect("missing response data");
+    for vuln in response_data.certify_vuln {
+        let mut products = HashSet::new();
+        let status = Status::Affected;
+        let justification = None;
+        products.insert(vuln.package.namespaces[0].names[0].name.clone());
+        let id = match vuln.vulnerability {
+            OSV(osv) => {
+                osv.osv_id[0].id.clone()
+            },
+            _ => {
+                String::from("NOT_SET")
+            }
+        };
 
+        //let tm = &vuln.time_scanned[0..36];
+        //let now_parsed = DateTime::parse_from_str("%Y-%m-%d %H:%M:%S%.f %z %Z", tm).unwrap();
+        //TODO parse &vuln.time_scanned
+        let now_parsed = Utc::now();
+
+        let statement = Statement {
+          vulnerability: Some(id.clone()),
+            vuln_description: None,
+            timestamp: Some(now_parsed.into()),
+            products: products.drain().collect(),
+            subcomponents: Vec::new(),
+            status,
+            status_notes: Some("Vulnerabilities found in Guac".into()),
+            justification,
+            impact_statement: None,
+            action_statement: Some(format!(
+                "Review {} for details on the appropriate action",
+                id.clone()
+            )),
+            action_statement_timestamp: Some(Utc::now()),
+        };
+        vex.statements.push(statement);
+    }
+
+    println!("{:#?}", vex);
     Ok(())
 
+}
+
+
+fn openvex() -> OpenVex {
+    OpenVex {
+        metadata: Metadata {
+            context: "https://openvex.dev/ns".to_string(),
+            id: format!(
+                "https://seedwing.io/ROOT/generated/{}",
+                uuid::Uuid::new_v4()
+            ),
+            author: "Seedwing Policy Engine".to_string(),
+            role: "Document Creator".to_string(),
+            timestamp: Some(Utc::now()),
+            version: format!("{}", "1"),
+            tooling: Some("Seedwing Policy Engine".to_string()),
+            supplier: Some("seedwing.io".to_string()),
+        },
+        statements: Vec::new(),
+    }
 }
