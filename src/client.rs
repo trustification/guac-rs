f595709ad2a48fa3fe06882c777639ed10a909bc
Dejan Bosanac <dbosanac@redhat.com>
2023-03-09 14:59:26 +0100

    Finish client example

--- src/client.rs
+++ src/client.rs
@@ -1,7 +1,11 @@
+use std::{collections::HashSet, sync::atomic::{Ordering, AtomicU64}};
+
+use chrono::{Utc, DateTime};
 use graphql_client::{GraphQLQuery, reqwest::post_graphql};
 use anyhow::*;
+use openvex::{OpenVex, Metadata, Statement, Status};
 
-use self::certify_vuln::{allCertifyVuln, PkgSpec};
+use self::certify_vuln::{allCertifyVuln, PkgSpec, AllCertifyVulnVulnerability::OSV};
 
 #[derive(GraphQLQuery)]
 #[graphql(
@@ -27,10 +31,71 @@ impl GuacClient {
     pub async fn certify_vuln(&self, pkg: PkgSpec) -> Result<Vec<allCertifyVuln>, anyhow::Error> {
         let variables = certify_vuln::Variables {
             package: Some(pkg)
-        };        
+        };
         let response_body = post_graphql::<CertifyVuln, _>(&self.client, self.url.to_owned(), variables).await?;
         let response_data = response_body.data.with_context(|| "No data found in response");
         Ok(response_data?.certify_vuln)
     }
 
+}
+
+static VERSION: AtomicU64 = AtomicU64::new(1);
+fn openvex() -> OpenVex {
+    OpenVex {
+        metadata: Metadata {
+            context: "https://openvex.dev/ns".to_string(),
+            id: format!(
+                "https://seedwing.io/ROOT/generated/{}",
+                uuid::Uuid::new_v4()
+            ),
+            author: "Seedwing Policy Engine".to_string(),
+            role: "Document Creator".to_string(),
+            timestamp: Some(Utc::now()),
+            version: format!("{}", VERSION.fetch_add(1, Ordering::Relaxed)),
+            tooling: Some("Seedwing Policy Engine".to_string()),
+            supplier: Some("seedwing.io".to_string()),
+        },
+        statements: Vec::new(),
+    }
+}
+
+pub fn vulns2vex(vulns: Vec<allCertifyVuln>) -> OpenVex {
+    let mut vex = openvex();
+
+    for vuln in vulns {
+        let mut products = HashSet::new();
+        let status = Status::Affected;
+        let justification = None;
+        products.insert(vuln.package.namespaces[0].names[0].name.clone());
+        let id = match vuln.vulnerability {
+            OSV(osv) => {
+                osv.osv_id[0].id.clone()
+            },
+            _ => {
+                String::from("NOT_SET")
+            }
+        };
+
+        let now_parsed = DateTime::parse_from_rfc3339(&vuln.time_scanned).unwrap();
+
+        let statement = Statement {
+          vulnerability: Some(id.clone()),
+            vuln_description: None,
+            timestamp: Some(now_parsed.into()),
+            products: products.drain().collect(),
+            subcomponents: Vec::new(),
+            status,
+            status_notes: Some("Vulnerabilities reported by Guac".into()),
+            justification,
+            impact_statement: None,
+            action_statement: Some(format!(
+                "Review {} for details on the appropriate action",
+                id.clone()
+            )),
+            action_statement_timestamp: Some(Utc::now()),
+        };
+        vex.statements.push(statement);        
+    }
+
+    vex
 }
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,14 @@
-use std::collections::HashSet;
-
-use chrono::{Utc, DateTime};
-use guac_rs::client::{GuacClient, certify_vuln::*, certify_vuln::AllCertifyVulnVulnerability::OSV};
-use openvex::{Metadata, OpenVex, Status, Statement};
+use guac_rs::client::{GuacClient, certify_vuln::*, vulns2vex};
 use packageurl::PackageUrl;
-use reqwest::Client;
-use graphql_client::{reqwest::post_graphql, GraphQLQuery};
 use anyhow::*;
 use std::str::FromStr;
 
-
-// #[derive(GraphQLQuery)]
-// #[graphql(
-//     schema_path = "src/schema.json",
-//     query_path = "src/query/certify_vuln.gql",
-//     response_derives = "Debug, Serialize, Deserialize"
-// )]
-// pub struct CertifyVuln;
+use colored_json::prelude::*;
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
-
     let purl = PackageUrl::from_str("pkg:pypi/django")?;
     //let purl = PackageUrl::from_str("pkg:maven/io.vertx/vertx-web@4.3.7")?;
-
-    println!("{:?} - {:?} - {:?}", purl.ty(), purl.namespace(), purl.name());
 
     let pkg = PkgSpec {
         type_: Some(purl.ty().to_string()),
@@ -39,64 +23,13 @@ async fn main() -> Result<(), anyhow::Error> {
     let guac = GuacClient::new("http://localhost:8080/query".to_string());
     let vulns = guac.certify_vuln(pkg).await?;
 
-    let mut vex = openvex();
+    let vex = vulns2vex(vulns);
 
-    for vuln in vulns {
-        let mut products = HashSet::new();
-        let status = Status::Affected;
-        let justification = None;
-        products.insert(vuln.package.namespaces[0].names[0].name.clone());
-        let id = match vuln.vulnerability {
-            OSV(osv) => {
-                osv.osv_id[0].id.clone()
-            },
-            _ => {
-                String::from("NOT_SET")
-            }
-        };
+    let out = serde_json::to_string(&vex)?.to_colored_json_auto()?;
 
-        let now_parsed = DateTime::parse_from_rfc3339(&vuln.time_scanned).unwrap();
-
-        let statement = Statement {
-          vulnerability: Some(id.clone()),
-            vuln_description: None,
-            timestamp: Some(now_parsed.into()),
-            products: products.drain().collect(),
-            subcomponents: Vec::new(),
-            status,
-            status_notes: Some("Vulnerabilities reported by Guac".into()),
-            justification,
-            impact_statement: None,
-            action_statement: Some(format!(
-                "Review {} for details on the appropriate action",
-                id.clone()
-            )),
-            action_statement_timestamp: Some(Utc::now()),
-        };
-        vex.statements.push(statement);
-    }
-
-    println!("{:#?}", vex);
+    println!("{}", out);
     Ok(())
 
 }
 
 
-fn openvex() -> OpenVex {
-    OpenVex {
-        metadata: Metadata {
-            context: "https://openvex.dev/ns".to_string(),
-            id: format!(
-                "https://seedwing.io/ROOT/generated/{}",
-                uuid::Uuid::new_v4()
-            ),
-            author: "Seedwing Policy Engine".to_string(),
-            role: "Document Creator".to_string(),
-            timestamp: Some(Utc::now()),
-            version: format!("{}", "1"),
-            tooling: Some("Seedwing Policy Engine".to_string()),
-            supplier: Some("seedwing.io".to_string()),
-        },
-        statements: Vec::new(),
-    }
-}
