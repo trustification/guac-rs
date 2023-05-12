8ef6cd850a87861e7d283589414ce3ed621f14d2
Dejan Bosanac <dbosanac@redhat.com>
2023-05-12 16:15:37 +0200

    Add --color option

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,8 +1,10 @@
+use std::str::FromStr;
+
 use anyhow::*;
-use clap::{Parser, Subcommand};
+use clap::{Parser, Subcommand, ColorChoice};
 use guac::{client::GuacClient};
 
-use colored_json::prelude::*;
+use colored_json::{prelude::*, Output};
 
 #[derive(Parser, Debug)]
 #[command(
@@ -19,6 +21,13 @@ pub struct Cli {
         default_value = "http://localhost:8080/query"
     )]
     pub(crate) guac_url: String,
+
+    #[arg(
+        short = 'c',
+        long = "color",
+        default_value = "auto",
+    )]
+    color: ColorChoice,
 
     #[command(subcommand)]
     pub(crate) command: Commands,
@@ -63,26 +72,35 @@ async fn main() -> Result<(), anyhow::Error> {
             purl
         } => {
             let deps = guac.get_dependencies(&purl).await?;
-            let out = serde_json::to_string(&deps)?.to_colored_json_auto()?;
+            let out = serde_json::to_string(&deps)?.to_colored_json(color_mode(cli.color))?;
             println!("{}", out);
         },
         Commands::Dependents { purl } => {
             let deps = guac.is_dependent(&purl).await?;
-            let out = serde_json::to_string(&deps)?.to_colored_json_auto()?;
+            let out = serde_json::to_string(&deps)?.to_colored_json(color_mode(cli.color))?;
             println!("{}", out);
         },
         Commands::Packages { purl } => {
             // e.g. "pkg:maven/io.vertx/vertx-web"
             let pkgs = guac.get_packages(&purl).await?;
-            let out = serde_json::to_string(&pkgs)?.to_colored_json_auto()?;
+            let out = serde_json::to_string(&pkgs)?.to_colored_json(color_mode(cli.color))?;
             println!("{}", out);
         },
         Commands::Vulnerabilities { purl } => {
             let vex = guac.certify_vuln_as_vex(&purl).await?;
-            let out = serde_json::to_string(&vex)?.to_colored_json_auto()?;
+            let out = serde_json::to_string(&vex)?.to_colored_json(color_mode(cli.color))?;
             println!("{}", out);
         }
     }
 
     Ok(())
+}
+
+
+fn color_mode(choice: ColorChoice) -> ColorMode {
+    match choice {
+        ColorChoice::Auto => ColorMode::Auto(Output::StdOut),
+        ColorChoice::Always => ColorMode::On,
+        ColorChoice::Never => ColorMode::Off,
+    }
 }
