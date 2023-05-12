6ecad4d9e39f4a840df15eb0098e1ad3b94f15a4
Dejan Bosanac <dbosanac@redhat.com>
2023-05-12 11:29:31 +0200

    Turn examples into cli

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,37 +1,88 @@
 use anyhow::*;
+use clap::{Parser, Subcommand};
 use guac::{client::GuacClient};
 
 use colored_json::prelude::*;
 
+#[derive(Parser, Debug)]
+#[command(
+    author,
+    version = env!("CARGO_PKG_VERSION"),
+    about = "Guac API CLI",
+    long_about = None
+)]
+pub struct Cli {
+
+    #[arg(
+        short = 'g',
+        long = "guac",
+        default_value = "http://localhost:8080/query"
+    )]
+    pub(crate) guac_url: String,
+
+    #[command(subcommand)]
+    pub(crate) command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// get all dependencies for the artifact
+    Dependencies {
+        /// Artifact purl
+        purl: String,
+    },
+    /// get all artifact that depend on this one
+    Dependents {
+        /// Artifact purl
+        purl: String,
+    },
+    /// get related packages 
+    Packages {
+        /// Artifact purl
+        purl: String,
+    },
+    /// get all certified 
+    Vulnerabilities {
+        /// Artifact purl
+        purl: String,
+    },
+}
+
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
-    let purl = "pkg:maven/io.vertx/vertx-web@4.3.7";
+    //let purl = "pkg:maven/io.vertx/vertx-web@4.3.7";
     //let purl = "pkg:deb/debian";
     //let purl = "pkg:pypi/django";
     //let purl = "pkg:rpm/redhat/openssl@1.1.1k-7.el8_6";
 
+    let cli = Cli::parse();
+    let guac = GuacClient::new(cli.guac_url);
 
-    let guac = GuacClient::new("http://localhost:8080/query".to_string());
-
-    //get dependencies
-    let deps = guac.get_dependencies(purl).await?;
-    let out = serde_json::to_string(&deps)?.to_colored_json_auto()?;
-    println!("{}", out);
-
-    //is dependent
-    let deps = guac.is_dependent(purl).await?;
-    let out = serde_json::to_string(&deps)?.to_colored_json_auto()?;
-    println!("{}", out);
-
-    //get packages
-    let pkgs = guac.get_packages("pkg:maven/io.vertx/vertx-web").await?;
-    let out = serde_json::to_string(&pkgs)?.to_colored_json_auto()?;
-    println!("{}", out);
-
-    //certify vulns
-    let vex = guac.certify_vuln_as_vex(purl).await?;
-    let out = serde_json::to_string(&vex)?.to_colored_json_auto()?;
-    println!("{}", out);
+    match cli.command {
+        Commands::Dependencies{
+            purl
+        } => {
+            let deps = guac.get_dependencies(&purl).await?;
+            let out = serde_json::to_string(&deps)?.to_colored_json_auto()?;
+            println!("{}", out);
+        },
+        Commands::Dependents { purl } => {
+            let deps = guac.is_dependent(&purl).await?;
+            let out = serde_json::to_string(&deps)?.to_colored_json_auto()?;
+            println!("{}", out);
+        },
+        Commands::Packages { purl } => {
+            // e.g. "pkg:maven/io.vertx/vertx-web"
+            let pkgs = guac.get_packages(&purl).await?;
+            let out = serde_json::to_string(&pkgs)?.to_colored_json_auto()?;
+            println!("{}", out);
+        },
+        Commands::Vulnerabilities { purl } => {
+            let vex = guac.certify_vuln_as_vex(&purl).await?;
+            let out = serde_json::to_string(&vex)?.to_colored_json_auto()?;
+            println!("{}", out);
+        }
+    }
 
     Ok(())
 }
