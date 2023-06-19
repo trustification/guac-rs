4a40c48e0e5338355f9b4adb7d0e550e56075872
Dejan Bosanac <dbosanac@redhat.com>
2023-06-19 12:54:53 +0200

    chore: refactor file collect command

--- cli/src/collect.rs
+++ cli/src/collect.rs
@@ -6,24 +6,50 @@ use guac::collector::{
     emitter::NatsEmitter,
 };
 
-#[derive(clap::Subcommand, Debug)]
+use clap::Subcommand;
+
+#[derive(Subcommand, Debug)]
 pub enum CollectCommand {
-    File { path: String },
-    S3 {},
+    File(FileCommand),
 }
 
 impl CollectCommand {
     pub async fn run(self) -> anyhow::Result<ExitCode> {
-        println!("Collecting ...");
+        match self {
+            Self::File(command) => command.run().await,
+        }
+    }
+}
 
-        let emitter = NatsEmitter::new("127.0.0.1:4222").await?;
+#[derive(Clone, Debug, clap::Parser)]
+#[command(rename_all_env = "SCREAMING_SNAKE_CASE")]
+pub struct FileConfig {
+    #[arg(short = 'n', long = "nats", default_value = "127.0.0.1:4222")]
+    pub(crate) nats_url: String,
+
+    path: String,
+}
+
+#[derive(clap::Args, Debug)]
+#[command(
+    about = "Run the file collector",
+    args_conflicts_with_subcommands = true
+)]
+pub struct FileCommand {
+    #[command(flatten)]
+    pub(crate) config: FileConfig,
+}
+
+impl FileCommand {
+    pub async fn run(self) -> anyhow::Result<ExitCode> {
+        println!("Collecting file {:?}", self.config.path);
+        let emitter = NatsEmitter::new(&self.config.nats_url).await?;
 
         let collector = FileCollector {
-            path: "example/seedwing-java-example.bom".to_string(),
+            path: self.config.path,
         };
 
         collector.run(emitter).await?;
-
         Ok(ExitCode::SUCCESS)
     }
 }
