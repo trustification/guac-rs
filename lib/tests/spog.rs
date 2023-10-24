b0b3d5c0d5e40408886650418d453ab716b9d1f1
Dejan Bosanac <dbosanac@redhat.com>
2023-10-24 09:58:50 +0200

    feat: add vulnerabilities query

--- lib/tests/spog.rs
+++ lib/tests/spog.rs
@@ -15,3 +15,20 @@ async fn product_by_cve() -> Result<(), anyhow::Error> {
 
     Ok(())
 }
+
+//TODO do proper testing
+// ./bin/guacone collect files --gql-addr http://localhost:8085/query ./rhel-7.9.z.json
+// ./bin/guacone collect files --gql-addr http://localhost:8085/query ./cve-2022-2284.json
+#[ignore]
+#[tokio::test]
+async fn find_vulnerability() -> Result<(), anyhow::Error> {
+    let client = GuacClient::new(&"http://localhost:8085/query");
+
+    let result = client
+        .semantic()
+        .find_vulnerability("pkg:guac/pkg/rhel-7.9.z@7.9.z")
+        .await?;
+    println!("result {:?}", result);
+
+    Ok(())
+}
