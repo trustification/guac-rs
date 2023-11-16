fb21d4fae116d88d1b0076cfdefb58cc472678d7
Dejan Bosanac <dbosanac@redhat.com>
2023-11-16 12:59:20 +0100

    feat: implement find_vulnerability_by_sbom_uri query

--- lib/tests/spog.rs
+++ lib/tests/spog.rs
@@ -32,3 +32,20 @@ async fn find_vulnerability() -> Result<(), anyhow::Error> {
 
     Ok(())
 }
+
+//TODO do proper testing
+// ./bin/guacone collect files --gql-addr http://localhost:8085/query ./rhel-7.9.z.json
+// ./bin/guacone collect files --gql-addr http://localhost:8085/query ./cve-2022-2284.json
+#[ignore]
+#[tokio::test]
+async fn find_vulnerability_by_sbom_uri() -> Result<(), anyhow::Error> {
+    let client = GuacClient::new(&"http://localhost:8085/query");
+
+    let result = client
+        .semantic()
+        .find_vulnerability_by_sbom_uri("https://access.redhat.com/security/data/sbom/beta/spdx/rhel-7.9.z-c98403ce-5e02-4278-98ec-b36ecd1f46a5", Some(0), Some(20))
+        .await?;
+    println!("{}", serde_json::to_string_pretty(&result).unwrap());
+
+    Ok(())
+}
