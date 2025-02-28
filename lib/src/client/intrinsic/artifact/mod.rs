75c90fa590430f4d1664dcf3da3ad2a1c99eea54
mrizzi <mrizzi@redhat.com>
2025-02-28 19:12:50 +0100

    TC-2279 has_sbom: subject artifact management
    Signed-off-by: mrizzi <mrizzi@redhat.com>

--- lib/src/client/intrinsic/artifact/mod.rs
+++ lib/src/client/intrinsic/artifact/mod.rs
@@ -1,8 +1,16 @@
 use crate::client::Id;
+use serde::{Deserialize, Serialize};
 
 #[derive(Default, Debug, Clone)]
 pub struct ArtifactSpec {
     pub id: Option<Id>,
     pub algorithm: Option<String>,
     pub digest: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+pub struct Artifact {
+    pub id: Id,
+    pub algorithm: String,
+    pub digest: String,
 }
